339972c49d10e54f793a4fb3d09f68e93aa14935
vrashab-dev <vrashab.dev@gamil.com>
2024-07-09 00:45:16 +0530

    Fifth Mini_Project_5

---
+++ script.js
@@ -0,0 +1,27 @@
+function ColorChange() {
+ const Hex="0123456789ABCDEF";
+ let CH ="#" 
+ for (let i = 0; i < 6; i++) {
+     CH += (Hex[Rand()]);  
+}
+ function Rand() {
+     return Math.floor(Math.random()*16);  
+ } 
+return CH;
+}
+let set;
+
+document.getElementById('Stop').addEventListener('click',()=>{
+     clearInterval(set);
+     set =null;
+})
+
+document.getElementById('Start').addEventListener('click',()=>{
+     function START() {
+     document.querySelector('#body').style.backgroundColor=ColorChange();
+     }
+     if(!set){//to prevent multiple call which increaces the speed of the color change.
+     set=setInterval(START,1500);
+     }
+})
+
