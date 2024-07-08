552462c0876ae0f7b77f4a5469cb84a07e67a69b
vrashab-dev <vrashab.dev@gamil.com>
2024-07-09 00:51:16 +0530

    Fifth Mini_Project_5

--- script.js
+++ script.js
@@ -1,27 +1,28 @@
 function ColorChange() {
- const Hex="0123456789ABCDEF";
- let CH ="#" 
- for (let i = 0; i < 6; i++) {
-     CH += (Hex[Rand()]);  
-}
- function Rand() {
-     return Math.floor(Math.random()*16);  
- } 
-return CH;
+  const Hex = "0123456789ABCDEF";
+  let CH = "#";
+  for (let i = 0; i < 6; i++) {
+    CH += Hex[Rand()];
+  }
+  function Rand() {
+    return Math.floor(Math.random() * 16);
+  }
+  return CH;
 }
 let set;
+let body = document.querySelector("#body");
+document.getElementById("Stop").addEventListener("click", () => {
+  clearInterval(set);
+  set = null;
+});
 
-document.getElementById('Stop').addEventListener('click',()=>{
-     clearInterval(set);
-     set =null;
-})
+document.getElementById("Start").addEventListener("click", () => {
+  function START() {
+    body.style.backgroundColor = ColorChange();
+  }
 
-document.getElementById('Start').addEventListener('click',()=>{
-     function START() {
-     document.querySelector('#body').style.backgroundColor=ColorChange();
-     }
-     if(!set){//to prevent multiple call which increaces the speed of the color change.
-     set=setInterval(START,1500);
-     }
-})
-
+  if (!set) {
+    //to prevent multiple call which increaces the speed of the color change.
+    set = setInterval(START, 1500);
+  }
+});
